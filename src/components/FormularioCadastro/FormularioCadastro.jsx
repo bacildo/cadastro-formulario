3d384b05d0b153959138f0c44795e3e708c6134d
bacildo <diogo.silva@m2msolutions.com.br>
2021-07-19 14:19:58 -0300

    inicio da separação condicional dos formularios

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import DadosDeEntrega from "./DadosDeEntrega";
 import DadosLoginUsuario from "./DadosLoginUsuario";
-
 import DadosPessoais from "./DadosPessoais";
 
 function FormularioCadastro({ onSubmitForm, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais onSubmitForm={onSubmitForm} validarCPF={validarCPF} />
-      <DadosLoginUsuario />
-      <DadosDeEntrega />
-    </>
-  );
+  const [momentoAtual, setMomentoAtual] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosLoginUsuario />;
+      case 1:
+        return (
+          <DadosPessoais onSubmitForm={onSubmitForm} validarCPF={validarCPF} />
+        );
+      case 2:
+        return <DadosDeEntrega />;
+      default:
+        return <Typography>Erro</Typography>;
+    }
+  }
+  return <>{formularioAtual(momentoAtual)}</>;
 }
+
+// <DadosDeEntrega />
+//<DadosPessoais onSubmitForm={onSubmitForm} validarCPF={validarCPF} />
+
+//
 
 export default FormularioCadastro;
